af30dc9923f7b4d50e5c8847c17563ab841b893c
LLexender <aleksandrdmitryevich@gmail.com>
2023-10-07 19:00:14 +0300

    added Espeon (3/6 pokemons)

---
+++ app/src/main/java/lab/pokemons/Espeon.java
@@ -0,0 +1,20 @@
+package lab.pokemons;
+
+import ru.ifmo.se.pokemon.*;
+import lab.moves.*;
+
+public class Espeon extends Pokemon {
+	public Espeon() {
+		super();
+		setStats(65, 65, 60, 130, 95, 110);
+		setType(Type.PSYCHIC);
+		setMove(new ThunderShock(), new LightScreen());
+	}
+
+	public Staryu(java.lang.String name, int level) {
+		super(name, level);
+		setStats(65, 65, 60, 130, 95, 110);
+		setType(Type.PSYCHIC);
+		setMove(new ThunderShock(), new LightScreen());
+	}
+}
