3429d307b433bbc38228eecec85aca6fe25bfe6c
LLexender <aleksandrdmitryevich@gmail.com>
2023-10-09 03:18:07 +0300

    Fixed Guillotine.move x2

--- app/src/main/java/lab/moves/Guillotine.java
+++ app/src/main/java/lab/moves/Guillotine.java
@@ -6,7 +6,7 @@ public class Guillotine extends PhysicalMove {
 	public Guillotine() {
 		super(Type.NORMAL, 100, 30);
 	}
-
+	
 	protected void applyOppEffects(Pokemon p) {
 		Effect e = new Effect().chance(0.3);
 		if (e.success()) {
